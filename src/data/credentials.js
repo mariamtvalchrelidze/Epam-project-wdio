454df8204e2def4899385432f80503a973c2a8d2
Mariam Tvaltchrelidze <mariamtvalchrelidze@gmail.com>
2025-06-27 20:54:18 +0400

    uncommented glitch user

--- src/data/credentials.js
+++ src/data/credentials.js
@@ -5,7 +5,7 @@ export default {
         { username: 'problem_user', success: true },
         { username: 'error_user', success: true },
         { username: 'visual_user', success: true },
-        // { username: 'performance_glitch_user', success: true },
+        { username: 'performance_glitch_user', success: true },
     ],
     
     password: 'secret_sauce',
